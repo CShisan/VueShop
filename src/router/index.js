996af813cee629d0316c01efca08c8c1972d7af7
CShisan <55305718+CShisan@users.noreply.github.com>
2020-08-02 14:22:29 +0800

    登录初步完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/Home', component: Home }
+  ]
+})
+
+// 挂载路由导航守卫(拦截未登录直接通过url访问路由资源的请求)
+router.beforeEach((to, from, next) => {
+  // to   代表将要访问的路径
+  // from 代表从哪个路径跳转来
+  // next 是一个函数,表示放行
+
+  if (to.path === '/login') return next()
+  const token = window.sessionStorage.getItem('token')
+  if (!token) return next('/login')
+  next()
 })
 
 export default router
